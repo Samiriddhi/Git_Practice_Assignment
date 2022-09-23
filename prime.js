e5899711ef7e83ba321c0b2462e03b70da14b385
abhishek1494k <abhi.jaiswal1494@gmail.com>
2022-09-23 15:32:57 +0530

    m2_code_fix

--- prime.js
+++ prime.js
@@ -3,14 +3,15 @@ function prime(num){
      let fac=0;
      for(let i=1;i<=num;i++){
        if(i%num==0){
-  fac++;
-    }
-  if(fac==2){
-  return "prime";
-  }else{
-"non_prime";
-  }
-    }
-   let x=prime(5);
-  console.log(x);
+	fac++;
+    	}
+  
+	if(fac==2){
+  	console.log("prime");
+  	}else	{	
+	console.log("non_prime");
+  	}
+    	}
+
+prime(13);
   
